37ece86de5871ba898a09b00a56a371433fc4775
RORingBBK <bbk.programmerz@gmail.com>
2016-12-06 09:38:07 +0545

    Final Output resolved

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root 'films#index'
   resources :films, except: [:new, :edit]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
